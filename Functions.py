25fe4c81de6ce359b3ecae582a8abce8d63b8840
Unknown <jrforsman@gmail.com>
2018-05-20 19:37:58 -0400

    removed implemented methods

--- Functions.py
+++ Functions.py
@@ -16,17 +16,6 @@ def createDeck():
     random.shuffle(deck)
     return deck
 
-def getValue(card):                                      #Assign value to card
-    if card['val'] == 'A':
-        val = 11
-    elif card['val'] == 'J' or card['val'] == 'Q' or card['val'] == 'K':
-        val = 10
-    else:
-        val = int(card['val'])
-
-    return val
-
-
 def hit(deck, hand):                                     #Hand could be either user or dealer hand
     hand.append(deck.pop(0))
 
@@ -42,49 +31,13 @@ def getNewHand(deck):                                    #Return a new hand of s
 def cleanUp(deck, userHand, dealerHand):                 #Empty the hands and make new deck
     userHand, dealerHand, deck = [], [], createDeck()
 
-
-def getHandValue(hand):                                  #Return the value of hand
-    HandValue = 0
-    numAce = 0
-
-    for card in hand:
-        HandValue += getValue(card)
-
-        if card['val'] == 'A':
-
-            numAce += 1
-
-    while HandValue > 21 and numAce > 0:
-
-        HandValue -= 10
-        numAce -= 1
-
-    return HandValue
-
-
-def isBlackjack(hand):                                   #Return true if hand is blackjack
-    return (len(hand) == 2 and getHandValue(hand) == 21)
-
-
-def canSplit(hand):                                      #Return True if hand can be split
-    return (len(hand) == 2 and hand[0]['val'] == hand[1]['val'])
-
-
-def canDouble(hand):                                     #Return True if hand can be doubled
-    return (len(hand) == 2 and getHandValue(hand) >=9 and getHandValue(hand) <= 11)
-
-
-def canInsure(hand):                                     #Return True if player can insure hand (first card face up)
-    return (len(hand) == 2 and hand[0]['val'] == 'A')
-
-
 def DealerLogic(deck, hand):
 
     if getHandValue(hand) >= 17:
         print('The dealer will stand')
 
     else:
-        while getHandValue(hand) <=16:
+        while getHandValue(hand) <= 16:
             hit(deck, hand)
             print('The dealer will hit')
 
