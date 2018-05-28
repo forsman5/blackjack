a32bba2b9afd1061224a2072a11c480805012c0b
Unknown <jrforsman@gmail.com>
2018-05-28 18:10:42 -0500

    fixed post request routing, need to attach csrf next

--- game/static/scripts/game_requests.js
+++ game/static/scripts/game_requests.js
@@ -1,32 +1,34 @@
+// TODO: replace urls with django urls
+
 // file containing methods for button presses on the game page
 
 function hit() {
-  var jqxhr = $.post(window.location.href + "/hit/", function (response) {
-    alert(response);
+  var jqxhr = $.post(window.location.href + "/hit", function (response) {
+    console.log(response);
     // TODO: display new game state returned
   });
 }
 
 function stand() {
-  var jqxhr = $.post(window.location.href + "/stand/", function (response) {
+  var jqxhr = $.post(window.location.href + "/stand", function (response) {
     // TODO: display new game state returned
   });
 }
 
 function insure() {
-  var jqxhr = $.post(window.location.href + "/insure/", function (response) {
+  var jqxhr = $.post(window.location.href + "/insure", function (response) {
     // TODO: display new game state returned
   });
 }
 
 function double() {
-  var jqxhr = $.post(window.location.href + "/double/", function (response) {
+  var jqxhr = $.post(window.location.href + "/double", function (response) {
     // TODO: display new game state returned
   });
 }
 
 function split() {
-  var jqxhr = $.post(window.location.href + "/split/", function (response) {
+  var jqxhr = $.post(window.location.href + "/split", function (response) {
     // TODO: display new game state returned
   });
 }
@@ -35,3 +37,15 @@ function split() {
 function disableButtons() {
   // TODO: implement
 }
+
+// register the buttons on page load
+$(document).ready(function() {
+  ids = ["hit", "stand", "insure", "double", "split"];
+  functions = [hit, stand, insure, double, split]
+  for (index in ids) {
+    x = document.getElementById(ids[index]);
+    if (x != null) {
+      x.addEventListener("click", functions[index]);
+    }
+  }
+});
